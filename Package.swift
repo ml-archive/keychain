1ed222b141399e7d587fce0323a623be72127e93
Casper Rasmussen <cr@nodes.dk>
2017-01-29 09:39:43 +0100

    Updating vapor forms, which update Vapor to 1.4

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/nodes-vapor/sugar.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/siemensikkema/vapor-jwt.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/bygri/vapor-forms.git", majorVersion:0, minor: 3),
+        .Package(url: "https://github.com/bygri/vapor-forms.git", majorVersion:0, minor: 4),
         .Package(url: "https://github.com/nodes-vapor/flash.git", majorVersion: 0, minor: 1),
     ]
 )
