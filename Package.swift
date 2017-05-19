8d5f668d94def1370221408d33127eaabc864008
Siemen Sikkema <siemen@unclutter.nl>
2017-05-19 10:56:48 +0200

    Update dependencies for Vapor 2

--- Package.swift
+++ Package.swift
@@ -3,11 +3,12 @@ import PackageDescription
 let package = Package(
     name: "JWTKeychain",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/jwt-provider.git", Version(1,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/bcrypt.git", Version(1,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/nodes-vapor/sugar.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/nodes-vapor/flash.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 2),
+        // TODO: update url once PR is accepted
+        .Package(url: "https://github.com/siemensikkema/jwt-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/bcrypt.git", majorVersion: 1),
+        .Package(url: "https://github.com/nodes-vapor/sugar.git", majorVersion: 2),
+        .Package(url: "https://github.com/nodes-vapor/flash.git", majorVersion: 1)
     ]
 )
