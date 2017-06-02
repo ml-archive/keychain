21a71a3379c5b6f92f2bdd90e57cf9eb3dbd70bf
Siemen Sikkema <siemen@unclutter.nl>
2017-06-02 17:03:45 +0100

    Remove mysql-provider dependency

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "JWTKeychain",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/jwt-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/validation.git", majorVersion: 1),
         .Package(url: "https://github.com/nodes-vapor/sugar.git", majorVersion: 2),
