5ae6602a17fd59eb56da8886705a277dbcb9d824
Siemen Sikkema <siemen@unclutter.nl>
2017-06-16 09:49:51 +0100

    Add missing packages

--- Package.swift
+++ Package.swift
@@ -6,6 +6,8 @@ let package = Package(
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/jwt-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/validation.git", majorVersion: 1),
-        .Package(url: "https://github.com/nodes-vapor/flash.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/fluent-provider", majorVersion: 1),
+        .Package(url: "https://github.com/nodes-vapor/flash.git", majorVersion: 1),
+        .Package(url: "https://github.com/harlanhaskins/Punctual.swift", majorVersion: 1)
     ]
 )
