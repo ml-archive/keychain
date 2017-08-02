593f87c619e8b43b584463aed41004720e3ce57d
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2017-08-02 11:20:26 +0200

    Add forms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/harlanhaskins/Punctual.swift", majorVersion: 1),
         .Package(url: "https://github.com/nodes-vapor/flash.git", majorVersion: 1),
+        .Package(url: "https://github.com/siemensikkema/forms.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/jwt-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
