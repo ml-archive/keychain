38ce77f42c181b9efad3a46574c127b7e47d049e
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2017-08-02 10:51:29 +0200

    Add leaf-provider

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .Package(url: "https://github.com/nodes-vapor/flash.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/jwt-provider.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/leaf.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/validation.git", majorVersion: 1),
     ]
