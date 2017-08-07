acc943bfb9990128155e4bf50cfa5c98854e6b5b
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2017-08-07 11:07:26 +0200

    Fix repo links

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "JWTKeychain",
     dependencies: [
-        .Package(url: "https://github.com/harlanhaskins/Punctual.swift", majorVersion: 1),
+        .Package(url: "https://github.com/harlanhaskins/Punctual.swift.git", majorVersion: 1),
         .Package(url: "https://github.com/nodes-vapor/flash.git", majorVersion: 1),
-        .Package(url: "https://github.com/siemensikkema/forms.git", majorVersion: 1),
+        .Package(url: "https://github.com/siemensikkema/forms.git", majorVersion: 0),
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/jwt-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
