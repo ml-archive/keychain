c7ee9e1e6a197de77748e54c196f6cc08370ae0b
Steffen D. Sommer <steffendsommer@icloud.com>
2017-08-22 16:51:24 +0100

    Update StoreRequest.swift

--- Sources/JWTKeychain/Requests/Api/Users/StoreRequest.swift
+++ Sources/JWTKeychain/Requests/Api/Users/StoreRequest.swift
@@ -12,7 +12,7 @@ public class StoreRequest: Form {
     public static let fieldset = Fieldset([
         "name": StringField(String.MinimumLengthValidator(characters: 2)),
         "email": StringField(String.EmailValidator(), UniqueFieldValidator<User>(column: "email", message: "Email already taken")),
-        "password": StringField(String.MinimumLengthValidator(characters: 6), RegexValidator(regex: "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])")),
+        "password": StringField(String.MinimumLengthValidator(characters: 6), RegexValidator(regex: "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])", message: "Password not strong enough. Must contain a number, a capital letter and a small letter.")),
         ], requiring: ["name", "email", "password"])
     
     
