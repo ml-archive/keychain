17c036e50f6aade1791f55f42364f4b228328238
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2017-09-28 10:34:16 +0200

    Sort protocol conformances in alphabetical order

--- Sources/JWTKeychain/Model/User.swift
+++ Sources/JWTKeychain/Model/User.swift
@@ -34,61 +34,11 @@ public final class User: Model, SoftDeletable, Timestampable {
     }
 }
 
-// MARK: Preparation
+// MARK: EmailAddressRepresentable
 
-extension User: Preparation {
-    public static func prepare(_ database: Database) throws {
-        try database.create(self) {
-            $0.id()
-            $0.string(Keys.email, unique: true)
-            $0.string(Keys.name)
-            $0.string(Keys.hashedPassword, optional: true)
-        }
-    }
-
-    public static func revert(_ database: Database) throws {
-        try database.delete(self)
-    }
-}
-
-// MARK: RowRepresentable
-
-extension User: RowRepresentable {
-    public convenience init(row: Row) throws {
-        try self.init(
-            email: row.get(Keys.email),
-            name: row.get(Keys.name),
-            hashedPassword: row.get(Keys.hashedPassword)
-        )
-    }
-
-    public func makeRow() throws -> Row {
-        var row = Row()
-
-        try row.set(Keys.email, email)
-        try row.set(Keys.name, name)
-        try row.set(Keys.hashedPassword, hashedPassword)
-
-        return row
-    }
-}
-
-// MARK: PasswordResettable
-
-extension User: PasswordResettable {
-    public static func extractPasswordResetInfo(
-        from request: Request,
-        isOptional: Bool
-    ) throws -> PasswordResetInfoType {
-        guard let json = request.json else {
-            throw JWTKeychainUserError.missingJSONOnRequest
-        }
-
-        return PasswordResetForm(
-            email: try json.get(Keys.email),
-            password: try json.get(Keys.password),
-            isOptional: isOptional
-        )
+extension User: EmailAddressRepresentable {
+    public var emailAddress: EmailAddress {
+        return EmailAddress(name: name, address: email)
     }
 }
 
@@ -103,18 +53,6 @@ extension User: JSONRepresentable {
         try json.set(Keys.name, name)
 
         return json
-    }
-}
-
-// MARK: NodeRepresentable
-
-extension User: NodeRepresentable {}
-
-// MARK: PasswordAuthenticatable
-
-extension User: PasswordAuthenticatable {
-    public static var passwordVerifier: PasswordVerifier? {
-        return bCryptHasher
     }
 }
 
@@ -153,6 +91,86 @@ extension User: JWTKeychainAuthenticatable {
     }
 }
 
+// MARK: NodeRepresentable
+
+extension User: NodeRepresentable {}
+
+// MARK: PasswordAuthenticatable
+
+extension User: PasswordAuthenticatable {
+    public static var passwordVerifier: PasswordVerifier? {
+        return bCryptHasher
+    }
+}
+
+// MARK: PasswordResettable
+
+extension User: PasswordResettable {
+    public static func extractPasswordResetInfo(
+        from request: Request,
+        isOptional: Bool
+        ) throws -> PasswordResetInfoType {
+        guard let json = request.json else {
+            throw JWTKeychainUserError.missingJSONOnRequest
+        }
+
+        return PasswordResetForm(
+            email: try json.get(Keys.email),
+            password: try json.get(Keys.password),
+            isOptional: isOptional
+        )
+    }
+}
+
+// MARK: PasswordUpdateable
+
+extension User: PasswordUpdateable {
+    public func updatePassword(to newPassword: String) throws {
+        try update(password: newPassword)
+    }
+}
+
+// MARK: PayloadAuthenticatable
+
+extension User: PayloadAuthenticatable {
+    public struct UserIdentifier: JSONInitializable {
+        let id: Identifier
+
+        public init(json: JSON) throws {
+            id = Identifier(try json.get(SubjectClaim.name) as Node)
+        }
+    }
+
+    public typealias PayloadType = UserIdentifier
+
+    public static func authenticate(
+        _ payload: PayloadType
+        ) throws -> User {
+        guard let user = try User.find(payload.id) else {
+            throw Abort.notFound
+        }
+
+        return user
+    }
+}
+
+// MARK: Preparation
+
+extension User: Preparation {
+    public static func prepare(_ database: Database) throws {
+        try database.create(self) {
+            $0.id()
+            $0.string(Keys.email, unique: true)
+            $0.string(Keys.name)
+            $0.string(Keys.hashedPassword, optional: true)
+        }
+    }
+
+    public static func revert(_ database: Database) throws {
+        try database.delete(self)
+    }
+}
+
 // MARK: RequestUpdateable
 
 extension User: RequestUpdateable {
@@ -160,11 +178,11 @@ extension User: RequestUpdateable {
         guard let json = request.json else {
             throw JWTKeychainUserError.missingJSONOnRequest
         }
-
+        
         let name = json[Keys.name]?.string
         let email = json[Keys.email]?.string
         let password = try extractNewPassword(json: json)
-
+        
         if
             let email = email,
             email != self.email,
@@ -172,12 +190,12 @@ extension User: RequestUpdateable {
         {
             // require old password as confirmation when updating email
             try verifyOldPassword(json: json)
-
+            
             let numberOfExistingUsersWithSameEmail = try makeQuery()
                 .filter(Keys.email, email)
                 .filter(idKey, .notEquals, id)
                 .count()
-
+            
             if numberOfExistingUsersWithSameEmail > 0 {
                 throw JWTKeychainUserError.userWithGivenEmailAlreadyExists
             }
@@ -220,43 +238,25 @@ extension User: RequestUpdateable {
     }
 }
 
-// MARK: EmailAddressRepresentable
+// MARK: RowRepresentable
 
-extension User: EmailAddressRepresentable {
-    public var emailAddress: EmailAddress {
-        return EmailAddress(name: name, address: email)
-    }
-}
-
-// MARK: PasswordUpdateable
-
-extension User: PasswordUpdateable {
-    public func updatePassword(to newPassword: String) throws {
-        try update(password: newPassword)
-    }
-}
-
-// MARK: PayloadAuthenticatable
-
-extension User: PayloadAuthenticatable {
-    public struct UserIdentifier: JSONInitializable {
-        let id: Identifier
-
-        public init(json: JSON) throws {
-            id = Identifier(try json.get(SubjectClaim.name) as Node)
-        }
+extension User: RowRepresentable {
+    public convenience init(row: Row) throws {
+        try self.init(
+            email: row.get(Keys.email),
+            name: row.get(Keys.name),
+            hashedPassword: row.get(Keys.hashedPassword)
+        )
     }
 
-    public typealias PayloadType = UserIdentifier
+    public func makeRow() throws -> Row {
+        var row = Row()
 
-    public static func authenticate(
-        _ payload: PayloadType
-    ) throws -> User {
-        guard let user = try User.find(payload.id) else {
-            throw Abort.notFound
-        }
+        try row.set(Keys.email, email)
+        try row.set(Keys.name, name)
+        try row.set(Keys.hashedPassword, hashedPassword)
 
-        return user
+        return row
     }
 }
 
