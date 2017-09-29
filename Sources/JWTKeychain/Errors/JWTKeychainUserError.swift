e2e62099e5dfaaa1f9af173ed920c87a8d3762a8
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2017-09-29 16:24:09 +0200

    Sort enum cases

--- Sources/JWTKeychain/Errors/JWTKeychainUserError.swift
+++ Sources/JWTKeychain/Errors/JWTKeychainUserError.swift
@@ -3,35 +3,35 @@ import HTTP
 import Vapor
 
 public enum JWTKeychainUserError: Error {
-    case missingJSONOnRequest
-    case missingEmail
-    case missingName
-    case missingPassword
-    case missingOldPassword
-    case userWithGivenEmailAlreadyExists
-    case passwordsDoNotMatch
     case invalidEmail
+    case missingEmail
+    case missingJSONOnRequest
+    case missingName
+    case missingOldPassword
+    case missingPassword
+    case passwordsDoNotMatch
+    case userWithGivenEmailAlreadyExists
 }
 
 extension JWTKeychainUserError: AbortError {
     public var reason: String {
         switch self {
-        case .missingJSONOnRequest:
-            return "Missing JSON on request."
-        case .missingEmail:
-            return "Missing value for 'email' in request."
-        case .missingName:
-            return "Missing value for 'name' in request."
-        case .missingPassword:
-            return "Missing value for 'password' in request."
-        case .missingOldPassword:
-            return "Missing value for 'oldPassword' in request."
-        case .userWithGivenEmailAlreadyExists:
-            return "A user with that email address already exists."
-        case .passwordsDoNotMatch:
-            return "Passwords do not match."
         case .invalidEmail:
             return "Email is not valid."
+        case .missingEmail:
+            return "Missing value for 'email' in request."
+        case .missingJSONOnRequest:
+            return "Missing JSON on request."
+        case .missingName:
+            return "Missing value for 'name' in request."
+        case .missingOldPassword:
+            return "Missing value for 'oldPassword' in request."
+        case .missingPassword:
+            return "Missing value for 'password' in request."
+        case .passwordsDoNotMatch:
+            return "Passwords do not match."
+        case .userWithGivenEmailAlreadyExists:
+            return "A user with that email address already exists."
         }
     }
 
