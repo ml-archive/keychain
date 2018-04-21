2b42506c75e7d32892413442b10b2418c807a33a
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-04-21 08:09:05 +0200

    Add Auth

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/nodes-vapor/sugar.git", .branch("vapor-3")),
-        .package(url: "https://github.com/vapor/jwt.git", .branch("master")),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc.3"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc.2"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2")
     ],
     targets: [
