bd6601cf129391aa413bcc1b62579e6c3827d2d2
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-04-22 22:57:38 +0200

    Allow searching users using any filter

--- Sources/JWTKeychain/Command/GeneratePasswordResetTokenCommand.swift
+++ Sources/JWTKeychain/Command/GeneratePasswordResetTokenCommand.swift
@@ -2,35 +2,39 @@ import Authentication
 import Command
 import Fluent
 
-public struct GeneratePasswordResetTokenCommand<U: Model & JWTAuthenticatable>:
+public struct GeneratePasswordResetTokenCommand<
+    U: JWTAuthenticatable
+>:
     Command
 where
-    U.Database: QuerySupporting,
-    U.ID: StringConvertible
+    U.Database: QuerySupporting
 {
-    public let arguments: [CommandArgument] = [.argument(name: Keys.userId)]
+    public let arguments: [CommandArgument] = [.argument(name: Keys.query)]
     public let options: [CommandOption] = []
 
     public let help = ["Generates a password reset token for a user with a given id."]
 
     private let databaseIdentifier: DatabaseIdentifier<U.Database>
+    private let makeFilter: (String) throws -> ModelFilter<U>
 
     public init(
-        databaseIdentifier: DatabaseIdentifier<U.Database>
+        databaseIdentifier: DatabaseIdentifier<U.Database>,
+        makeFilter: @escaping (String) throws -> ModelFilter<U>
     ) {
         self.databaseIdentifier = databaseIdentifier
+        self.makeFilter = makeFilter
     }
 
     public func run(using context: CommandContext) throws -> Future<Void> {
-        let userId = try U.ID.convertFromString(context.argument(Keys.userId))
-
         let signer = try context.container.make(JWTKeychainConfig.self).resetPasswordTokenSigner
 
         return context
             .container
             .withPooledConnection(to: databaseIdentifier) { connection in
                 try U
-                    .find(userId, on: connection)
+                    .query(on: connection)
+                    .filter(self.makeFilter(context.argument(Keys.query)))
+                    .first()
                     .unwrap(or: JWTKeychainError.userNotFound)
                     .flatMap(to: String.self) { user in
                         user.signToken(using: signer, on: connection)
@@ -42,6 +46,17 @@ where
     }
 }
 
+extension GeneratePasswordResetTokenCommand where U.ID: StringConvertible {
+    public init(
+        databaseIdentifier: DatabaseIdentifier<U.Database>
+    ) {
+        self.databaseIdentifier = databaseIdentifier
+        self.makeFilter = { query -> ModelFilter<U> in
+            try U.idKey == U.ID.convertFromString(query)
+        }
+    }
+}
+
 private enum Keys {
-    static let userId = "userId"
+    static let query = "query"
 }
