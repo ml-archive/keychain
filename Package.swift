35ff5d23bca48ebe45b71fc8507a985478bdf808
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-04-27 09:48:25 +0200

    Use any rc

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/nodes-vapor/sugar.git", .branch("vapor-3")),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc.3"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc.2"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc")
     ],
     targets: [
         .target(
