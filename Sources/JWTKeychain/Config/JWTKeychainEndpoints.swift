1909017f64dbb44c9320e8dfa7abbe66ee918263
Steffen D. Sommer <steffendsommer@icloud.com>
2018-05-30 10:32:08 +0200

    Art be gone

--- Sources/JWTKeychain/Config/JWTKeychainEndpoints.swift
+++ Sources/JWTKeychain/Config/JWTKeychainEndpoints.swift
@@ -1,42 +1,42 @@
 import Routing
 
 public struct JWTKeychainEndpoints {
-    public let login    : String
-    public let me       : String
-    public let register : String
-    public let token    : String
-    public let update   : String
+    public let login: String
+    public let me: String
+    public let register: String
+    public let token: String
+    public let update: String
 
     /// Endpoints to use by provider when registering routes.
     ///
     /// - Parameters:
-    ///   - login   : login endpoint (POST)
-    ///   - me      : me endpoint (GET)
+    ///   - login: login endpoint (POST)
+    ///   - me: me endpoint (GET)
     ///   - register: register endpoint (POST)
-    ///   - token   : token refresh endpoint (POST)
-    ///   - update  : user update endpoint (PATCH)
+    ///   - token: token refresh endpoint (POST)
+    ///   - update: user update endpoint (PATCH)
     public init(
-        login   : String,
-        me      : String,
+        login: String,
+        me: String,
         register: String,
-        token   : String,
-        update  : String
+        token: String,
+        update: String
     ) {
-        self.login      = login
-        self.me         = me
-        self.register   = register
-        self.token      = token
-        self.update     = update
+        self.login = login
+        self.me = me
+        self.register = register
+        self.token = token
+        self.update = update
     }
 
     public static var `default`: JWTKeychainEndpoints {
         let users = "users"
         return .init(
-            login   : users + "/login",
-            me      : users + "/me",
+            login: users + "/login",
+            me: users + "/me",
             register: users,
-            token   : users + "/token",
-            update  : users + "/me"
+            token: users + "/token",
+            update: users + "/me"
         )
     }
 }
